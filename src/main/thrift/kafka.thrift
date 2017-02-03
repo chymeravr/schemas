07fae250635ddcc4af08dcfc4f087609ea0708a1
rubbal <rubbal@chymeravr.com>
2017-02-03 12:28:00 +0530

    Consistent naming

--- src/main/thrift/kafka.thrift
+++ src/main/thrift/kafka.thrift
@@ -2,7 +2,7 @@ namespace java com.chymeravr.schemas.kafka
 include "serving.thrift"
 include "eventreceiver.thrift"
 
-struct JoinedEvent {
+struct AttributedEvent {
     1: required serving.ServingLog servingLog;
     2: required eventreceiver.EventLog eventLog;
 }
