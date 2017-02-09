9d58f1652dfd0446b800e21ed8473d7929c0e03b
rubbal <rubbal@chymeravr.com>
2017-02-09 10:44:00 +0530

    Add eventreceiver request and response objects

--- src/main/thrift/eventreceiver.thrift
+++ src/main/thrift/eventreceiver.thrift
@@ -18,6 +18,29 @@ struct AdServingMeta {
     2: required i32 instanceId;
 }
 
+struct RuntimeAdMeta {
+    1: required string servingId;
+    2: required i32 instanceId;
+}
+
+struct SDKEvent {
+    1: required i64 timestamp;
+    2: required EventType eventType;
+    3: required RuntimeAdMeta adMeta;
+    4: optional map<string, string> paramsMap;
+}
+
+struct EventPing {
+    1: required i64 timestamp;
+    2: required i16 sdkVersion;
+    3: required string appId;
+    4: required list<SDKEvent> events;
+}
+
+struct EventResponse {
+
+}
+
 struct EventLog {
     1: required i64 timestamp;
     2: required string appId;
